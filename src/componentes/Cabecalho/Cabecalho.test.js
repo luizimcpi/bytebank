fb2b6687511e59a27c069a648cf4c8062902de10
Luiz Henrique <luizhenrique.se@gmail.com>
2025-04-06 18:41:45 -0300

    chore: creating Cabecalho logged user test scenario

--- src/componentes/Cabecalho/Cabecalho.test.js
+++ src/componentes/Cabecalho/Cabecalho.test.js
@@ -1,4 +1,15 @@
+import { render, screen } from '@testing-library/react';
+
+import Cabecalho from './index';
+
 test("Primeiro teste", () => {
     const numero = 10;
     expect(numero).toBe(10);
 })
+
+
+test('Deve renderizar o nome do usuário logado', () => {
+    render(<Cabecalho />);
+    const nomeUsuario = screen.getByText('Joana Fonseca Gomes');
+    expect(nomeUsuario).toBeInTheDocument();
+});
