977eb706d23ca1188c1cd3d202e754253c06d414
Luiz Henrique <luizhenrique.se@gmail.com>
2025-04-06 18:43:11 -0300

    chore: menu test scenario

---
+++ src/componentes/Menu/Menu.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react';
+
+import Menu from './index';
+
+test('Deve renderizar um link para a página inicial', () => {
+  render(<Menu />);
+  const linkPaginaInicial = screen.getByText('Inicial');
+  expect(linkPaginaInicial).toBeInTheDocument();
+});
