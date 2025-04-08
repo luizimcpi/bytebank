7744d5a885488813fe31e915539d5431dd10a3eb
Luiz Henrique <luizhenrique.se@gmail.com>
2025-04-07 22:54:56 -0300

    chore: add saldo test scenarios

--- src/componentes/Principal/Saldo/index.jsx
+++ src/componentes/Principal/Saldo/index.jsx
@@ -1,6 +1,6 @@
+import { ReactComponent as Icone } from './icone-olho.svg';
 import React from 'react';
 import estilos from './Saldo.module.css';
-import { ReactComponent as Icone } from './icone-olho.svg';
 
 export default function Saldo({ saldo }) {
   return (
@@ -11,7 +11,7 @@ export default function Saldo({ saldo }) {
       </div>
       <div className={estilos.divisor} />
       <p className={estilos.conta}>Conta corrente</p>
-      <p className={estilos.valor}>{`R$ ${saldo}`}</p>
+      <p data-testid="saldo" className={estilos.valor}>{`R$ ${saldo}`}</p>
     </div>
   );
 }
